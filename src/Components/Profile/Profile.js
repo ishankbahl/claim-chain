9a2bd7d9fa62054ead15bcbaa94957b2213e30ae
Shubham <imshubhamsingh97@gmail.com>
2018-05-06 01:47:55 +0530

    fixed hospital component

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,98 +1,98 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Card, { CardContent, CardMedia } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import LocalHospital from 'material-ui-icons/LocalHospital';
-import PersonPin from "material-ui-icons/PersonPin";
+import PersonPin from 'material-ui-icons/PersonPin';
 
 const styles = {
-    card: {
-      maxWidth: "345px",
-    },
-    media: {
-      height: 200,
-      width: "100%"
-    },
-    span: {
-        padding: "5px",
-    },
-    link: {
-        textDecoration: "none",
-    },
-    doctor: {
-        float: "left",
-    },
-    hospital: {
-        float: "right",
-    },
-    profile: {
-        display: "inline-block",
-        paddingRight: "25px",
-        paddingLeft: "25px",
-    }
-  };
-  
+  card: {
+    maxWidth: '645px'
+  },
+  media: {
+    height: 200,
+    width: '100%'
+  },
+  span: {
+    padding: '5px'
+  },
+  link: {
+    textDecoration: 'none'
+  },
+  doctor: {
+    float: 'left'
+  },
+  hospital: {
+    float: 'right'
+  },
+  profile: {
+    display: 'inline-block',
+    padding: '25px',
+    width: '330px'
+  }
+};
 
 function Profile(props) {
-    const { classes,
-         image,
-         name,
-         age,
-         timeOfDeath,
-         causeOfDeath,
-         doctor,
-         hospital,
-         } = props;
-    return (
-        <div className={ classes.profile } >
-        <Card className={classes.card}>
-            <CardMedia
-                className={classes.media}
-                image={image}
-                title="Image of Person"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="headline" component="h2">
-                    {name}
-                </Typography>
-                <Typography variant="subheading" color="textSecondary">
-                    Aged {age}
-                </Typography>
-                <span className={ classes.span } ></span>
-                <Typography variant="subheading" color="textSecondary">
-                    <b>Time of Death:</b> {timeOfDeath}
-                </Typography>
-                <Typography variant="subheading" color="textSecondary">
-                    <b>Cause of Death:</b> {causeOfDeath}
-                </Typography>
-                <span className={ classes.span } ></span>
-                <hr />
-                <Typography variant="subheading" color="textSecondary">
-                    <PersonPin className={classes.doctor} />
-                    <span className={classes.doctor} >{doctor}</span>
-                </Typography>
-                <Typography variant="subheading" color="textSecondary">
-                    <span className={classes.hospital} >{hospital}</span>
-                    <LocalHospital className={classes.hospital} />
-                </Typography>
-                <span className={ classes.span } ></span>
-            </CardContent>
-        </Card>
-        </div>
-    );
+  const {
+    classes,
+    image,
+    name,
+    age,
+    timeOfDeath,
+    causeOfDeath,
+    doctor,
+    hospital
+  } = props;
+  return (
+    <div className={classes.profile}>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={image}
+          title="Image of Person"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="headline" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="subheading" color="textSecondary">
+            Aged {age}
+          </Typography>
+          <span className={classes.span} />
+          <Typography variant="subheading" color="textSecondary">
+            <b>Time of Death:</b> {timeOfDeath}
+          </Typography>
+          <Typography variant="subheading" color="textSecondary">
+            <b>Cause of Death:</b> {causeOfDeath}
+          </Typography>
+          <span className={classes.span} />
+          <hr />
+          <Typography variant="subheading" color="textSecondary">
+            <PersonPin className={classes.doctor} />
+            <span className={classes.doctor}>{doctor}</span>
+          </Typography>
+          <Typography variant="subheading" color="textSecondary">
+            <span className={classes.hospital}>{hospital}</span>
+            <LocalHospital className={classes.hospital} />
+          </Typography>
+          <span className={classes.span} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 Profile.propTypes = {
-    classes: PropTypes.object.isRequired,
-    name: PropTypes.string.isRequired,
-    age: PropTypes.string.isRequired,
-    timeOfDeath: PropTypes.string.isRequired,
-    causeOfDeath: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    doctor: PropTypes.string.isRequired,
-    hospital: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  age: PropTypes.string.isRequired,
+  timeOfDeath: PropTypes.string.isRequired,
+  causeOfDeath: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  doctor: PropTypes.string.isRequired,
+  hospital: PropTypes.string.isRequired
 };
-  
+
 export default withStyles(styles)(Profile);
--- src/data.js
+++ src/data.js
@@ -1,82 +1,85 @@
-export const data = [{
-    name: "Matt Giampietro",
-    age: "46",
-    image: "https://semantic-ui.com/images/avatar2/large/matthew.png",
-    timeOfDeath: "20/6/12",
-    causeOfDeath: "Murder",
-    doctor: "Dr. BR Ambedkar",
-    hospital: "HR Hospital"
-},
-{
-    name: "Matt Giampietro",
-    age: "46",
-    image: "https://semantic-ui.com/images/avatar2/large/matthew.png",
-    timeOfDeath: "20/6/12",
-    causeOfDeath: "Murder",
-    doctor: "Dr. BR Ambedkar",
-    hospital: "HR Hospital"
-},
-{
-    name: "Matt Giampietro",
-    age: "46",
-    image: "https://semantic-ui.com/images/avatar2/large/matthew.png",
-    timeOfDeath: "20/6/12",
-    causeOfDeath: "Murder",
-    doctor: "Dr. BR Ambedkar",
-    hospital: "HR Hospital"
-},
-{
-    name: "Matt Giampietro",
-    age: "46",
-    image: "https://semantic-ui.com/images/avatar2/large/matthew.png",
-    timeOfDeath: "20/6/12",
-    causeOfDeath: "Murder",
-    doctor: "Dr. BR Ambedkar",
-    hospital: "HR Hospital"
-},
-{
-    name: "Matt Giampietro",
-    age: "46",
-    image: "https://semantic-ui.com/images/avatar2/large/matthew.png",
-    timeOfDeath: "20/6/12",
-    causeOfDeath: "Murder",
-    doctor: "Dr. BR Ambedkar",
-    hospital: "HR Hospital"
-},
-{
-    name: "Matt Giampietro",
-    age: "46",
-    image: "https://semantic-ui.com/images/avatar2/large/matthew.png",
-    timeOfDeath: "20/6/12",
-    causeOfDeath: "Murder",
-    doctor: "Dr. BR Ambedkar",
-    hospital: "HR Hospital"
-},
+export const data = [
+  {
+    name: 'Matt Giampietro',
+    age: '46',
+    image: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+    timeOfDeath: '20/6/12',
+    causeOfDeath: 'Murder',
+    doctor: 'Dr. BR Ambedkar',
+    hospital: 'HR Hospital'
+  },
+  {
+    name: 'Matt Giampietro',
+    age: '46',
+    image: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+    timeOfDeath: '20/6/12',
+    causeOfDeath: 'Murder',
+    doctor: 'Dr. BR Ambedkar',
+    hospital: 'HR Hospital'
+  },
+  {
+    name: 'Matt Giampietro',
+    age: '46',
+    image: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+    timeOfDeath: '20/6/12',
+    causeOfDeath: 'Murder',
+    doctor: 'Dr. BR Ambedkar',
+    hospital: 'HR Hospital'
+  },
+  {
+    name: 'Matt Giampietro',
+    age: '46',
+    image: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+    timeOfDeath: '20/6/12',
+    causeOfDeath: 'Murder',
+    doctor: 'Dr. BR Ambedkar',
+    hospital: 'HR Hospital'
+  },
+  {
+    name: 'Matt Giampietro',
+    age: '46',
+    image: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+    timeOfDeath: '20/6/12',
+    causeOfDeath: 'Murder',
+    doctor: 'Dr. BR Ambedkar',
+    hospital: 'HR Hospital'
+  },
+  {
+    name: 'Matt Giampietro',
+    age: '46',
+    image: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+    timeOfDeath: '20/6/12',
+    causeOfDeath: 'Murder',
+    doctor: 'Dr. BR Ambedkar',
+    hospital: 'HR Hospital'
+  }
 ];
-  
-export const userType = [{
-    name: "Insurer",
-    description: "Insurer saves time managing logistics so they can do better things",
-    Component: "AccountBalance"
-},
-{
-    name: "Insurer",
-    description: "Insurer saves time managing logistics so they can do better things",
-    Component: "AccountBalance"
-},
-{
-    name: "Insurer",
-    description: "Insurer saves time managing logistics so they can do better things",
-    Component: "AccountBalance"
-},
-{
-    name: "Insurer",
-    description: "Insurer saves time managing logistics so they can do better things",
-    Component: "AccountBalance"
-},
-{
-    name: "Insurer",
-    description: "Insurer saves time managing logistics so they can do better things",
-    Component: "AccountBalance"
-},
+
+export const userType = [
+  {
+    name: 'Insurer',
+    description:
+      'Insurer saves time managing logistics so they can do better things',
+    Component: 'AccountBalance'
+  },
+  {
+    name: 'Nominee',
+    description: 'Simple claims and processing for nominess to reduce trauma',
+    Component: 'AccountBalance'
+  },
+  {
+    name: 'Hospital',
+    description: 'Simple claims and processing for hospital',
+    Component: 'Hospital'
+  },
+  {
+    name: 'Government Body',
+    description: 'Civic bodies cooperate with us fully to make life convenient',
+    Component: 'Government'
+  },
+  {
+    name: 'Burial Site',
+    description: 'Trusted Partners make sure your data is safe and secure',
+    Component: 'Burial'
+  }
 ];
